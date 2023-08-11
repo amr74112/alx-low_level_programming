e91ef3f1848b7e6df45db73dba62ee1e542a1693
amr74112 <amr@example.com>
2023-08-10 21:25:06 -0700

    this is amr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  * Return: Alawy 0 (su)
  */
 int main (void){
-puts("\ "Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
