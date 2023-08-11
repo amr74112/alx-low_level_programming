55187beeaaf42c8c0a8fff5a23a68e11c5848374
amr74112 <amr@example.com>
2023-08-10 21:35:11 -0700

    this is amr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * main-Entry point
+ * main - Entry point
  *
  * Description: A C program that prints with put function
  *
- * Return: Alawy 0 (su)
+ * Return: Alawy 0 (success)
  */
 int main(void){
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\"");
 return(0);
 }
