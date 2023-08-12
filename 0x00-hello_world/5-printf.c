6a8e9b467726c647dd77bcdb32baf8865b8c3cd6
amr74112 <amr@example.com>
2023-08-12 04:09:16 -0700

    this is amr

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
  *
  * Description: A C program that prints with printf function
  *
- * Return: Alawy 0 (Su)
+ * Return: Alawy 0 (Success)
 */
 int main (void)
 {
-printf("with proper grammar,but the outcome is a piece of art,\n");
-return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
